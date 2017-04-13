3a12ce221ace3a93058337150cbf1f491974fa01
Zachary Murphy <ztmurphy21@gmail.com>
2017-04-13 17:06:00 -0400

    asssignment number 4

---
+++ Ebook Solution/Ebook Project/Main Form.Designer.vb
@@ -0,0 +1,143 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.lstEbooks = New System.Windows.Forms.ListBox()
+        Me.btnAdd = New System.Windows.Forms.Button()
+        Me.btnRemove = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1,System.ComponentModel.ISupportInitialize).BeginInit
+        Me.SuspendLayout
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = true
+        Me.Label1.Location = New System.Drawing.Point(220, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(38, 20)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Title"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = true
+        Me.Label2.Location = New System.Drawing.Point(532, 9)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(54, 20)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Author"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = true
+        Me.Label3.Location = New System.Drawing.Point(798, 10)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(41, 20)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Price"
+        '
+        'lstEbooks
+        '
+        Me.lstEbooks.Font = New System.Drawing.Font("Courier New", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0,Byte))
+        Me.lstEbooks.FormattingEnabled = true
+        Me.lstEbooks.ItemHeight = 16
+        Me.lstEbooks.Location = New System.Drawing.Point(191, 33)
+        Me.lstEbooks.Name = "lstEbooks"
+        Me.lstEbooks.Size = New System.Drawing.Size(669, 212)
+        Me.lstEbooks.Sorted = true
+        Me.lstEbooks.TabIndex = 6
+        '
+        'btnAdd
+        '
+        Me.btnAdd.Location = New System.Drawing.Point(312, 263)
+        Me.btnAdd.Name = "btnAdd"
+        Me.btnAdd.Size = New System.Drawing.Size(157, 31)
+        Me.btnAdd.TabIndex = 0
+        Me.btnAdd.Text = "&Add an eBook"
+        Me.btnAdd.UseVisualStyleBackColor = true
+        '
+        'btnRemove
+        '
+        Me.btnRemove.Location = New System.Drawing.Point(475, 263)
+        Me.btnRemove.Name = "btnRemove"
+        Me.btnRemove.Size = New System.Drawing.Size(157, 31)
+        Me.btnRemove.TabIndex = 1
+        Me.btnRemove.Text = "&Remove an eBook"
+        Me.btnRemove.UseVisualStyleBackColor = true
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(638, 263)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(71, 31)
+        Me.btnExit.TabIndex = 2
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = true
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.Ebook_Project.My.Resources.Resources.ebookOwl
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(162, 233)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 7
+        Me.PictureBox1.TabStop = false
+        '
+        'frmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8!, 20!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(881, 317)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnRemove)
+        Me.Controls.Add(Me.btnAdd)
+        Me.Controls.Add(Me.lstEbooks)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("Segoe UI", 11.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0,Byte))
+        Me.Margin = New System.Windows.Forms.Padding(4, 5, 4, 5)
+        Me.Name = "frmMain"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "eBooks"
+        CType(Me.PictureBox1,System.ComponentModel.ISupportInitialize).EndInit
+        Me.ResumeLayout(false)
+        Me.PerformLayout
+
+End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents lstEbooks As ListBox
+    Friend WithEvents btnAdd As Button
+    Friend WithEvents btnRemove As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents PictureBox1 As PictureBox
+End Class
---
+++ Ebook Solution/Ebook Project/Main Form.vb
@@ -0,0 +1,89 @@
+﻿' Name:         Ebook Project
+' Purpose:      Adds and deletes list box entries
+'               Reads information from a sequential access file
+'               Writes information to a sequential access file
+' Programmer:   Zach Murphy on 4-13-2017
+
+Option Explicit On
+Option Strict On
+Option Infer Off
+
+Public Class frmMain
+
+    Private Sub frmMain_Load(sender As Object, e As EventArgs) Handles Me.Load
+        ' fills the list box with data from 
+        ' a sequential access file
+        Dim inFile As IO.StreamReader
+        Dim strInfo As String
+
+        'verify that the file exists
+        If IO.File.Exists("Ebooks.txt") Then
+            'open the file for input
+            inFile = IO.File.OpenText("Ebooks.txt")
+            'process loop instructions until end of file
+            Do Until inFile.Peek = -1
+                strInfo = inFile.ReadLine
+                lstEbooks.Items.Add(strInfo)
+            Loop
+            inFile.Close()
+            'select the first line in the list box
+            lstEbooks.SelectedIndex = 0
+        Else
+            MessageBox.Show("Can't find the Ebooks.txt file", "eBooks", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End If
+
+    End Sub
+
+    Private Sub btnAdd_Click(sender As Object, e As EventArgs) Handles btnAdd.Click
+        ' adds eBook information to the list box
+
+        ' declare variables
+        Dim strTitle As String
+        Dim strAuthor As String
+        Dim strPrice As String
+        Dim strConcatenatedInfo As String
+        Dim dblPrice As Double
+        ' get the eBook information
+        strTitle = InputBox("Title:", "eBooks")
+        strAuthor = InputBox("Author:", "eBooks")
+        strPrice = InputBox("Price:", "eBooks")
+        ' format the price, then concatenate the input
+        ' items, using 40 characters for the title, 
+        ' 35 characters for the author, and 5 
+        ' characters for the price
+        Double.TryParse(strPrice, dblPrice)
+        strPrice = dblPrice.ToString("n2")
+        strConcatenatedInfo = strTitle.PadRight(40) & strAuthor.PadRight(35) & strPrice.PadLeft(5)
+        ' add the information to the list box
+        lstEbooks.Items.Add(strConcatenatedInfo)
+    End Sub
+
+    Private Sub btnRemove_Click(sender As Object, e As EventArgs) Handles btnRemove.Click
+        ' removes the selected line from the list box
+
+        ' if a line is selected, remove the line
+        If lstEbooks.SelectedIndex <> -1 Then
+            lstEbooks.Items.RemoveAt(lstEbooks.SelectedIndex)
+        End If
+    End Sub
+
+    Private Sub frmMain_FormClosing(sender As Object, e As FormClosingEventArgs) Handles Me.FormClosing
+        ' save the list box information
+
+        ' declare a StreamWriter variable
+        Dim outFile As IO.StreamWriter
+        ' open the file for output
+        outFile = IO.File.CreateText("Ebooks.txt")
+        ' write each line in the list box
+        For intIndex As Integer = 0 To lstEbooks.Items.Count - 1
+            outFile.WriteLine(lstEbooks.Items(intIndex))
+        Next intIndex
+
+        ' close the file
+        outFile.Close()
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+End Class
