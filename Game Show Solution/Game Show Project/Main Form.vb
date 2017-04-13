b900b204442be5b990afa05191ce3d7ad4a34e37
Zachary Murphy <ztmurphy21@gmail.com>
2017-04-13 16:31:15 -0400

    assignment 2 and 3

---
+++ Game Show Solution/Game Show Project/Main Form.vb
@@ -0,0 +1,62 @@
+﻿' Name:         Game Show Project
+' Purpose:      Write names to and read names from a sequential access file
+' Programmer:   Zach Murphy on 4-13-2017
+
+Option Explicit On
+Option Strict On
+Option Infer Off
+
+Public Class frmMain
+
+    Private Sub btnWrite_Click(sender As Object, e As EventArgs) Handles btnWrite.Click
+        ' writes a name to a sequential access file
+        ' declare a StreamWriter variable
+        Dim outFile As IO.StreamWriter
+
+        ' open the file for append
+        outFile = IO.File.AppendText("contestants.txt")
+        ' write the name on a separate line in the file
+        outFile.WriteLine(txtName.Text)
+        ' close the file
+        outFile.Close()
+        ' clear the list box and then set the focus
+        lstContestants.Items.Clear()
+        txtName.Focus()
+    End Sub
+
+    Private Sub btnRead_Click(sender As Object, e As EventArgs) Handles btnRead.Click
+        ' reads names from a sequential access file 
+        ' and displays them in the interface
+
+        ' declare variables
+        Dim inFile As IO.StreamReader
+        Dim strName As String
+        ' clear previous names from the list box
+        lstContestants.Items.Clear()
+
+        ' determine whether the file exists
+        If IO.File.Exists("contestants.txt") Then
+            'open the file for input
+            inFile = IO.File.OpenText("contestants.txt")
+            'process loop insturctions until end of file
+            Do Until inFile.Peek = -1
+                'read a name
+                strName = inFile.ReadLine
+                'add name to list box
+                lstContestants.Items.Add(strName)
+            Loop
+            'closes the file
+            inFile.Close()
+        Else
+            MessageBox.Show("Can't find the file", "Game Show Contestants", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End If
+    End Sub
+
+    Private Sub txtName_Enter(sender As Object, e As EventArgs) Handles txtName.Enter
+        txtName.SelectAll()
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+End Class
---
+++ Paper Solution-Structure/Paper Project/Main Form.vb
@@ -0,0 +1,58 @@
+﻿' Name:         Paper Project
+' Purpose:      Displays the price of an item
+' Programmer:   Zach Murphy on 4-13-2017
+
+Option Explicit On
+Option Strict On
+Option Infer Off
+
+Public Class frmMain
+    Structure ProductInfo
+        Public strId As String
+        Public dblPrice As Double
+    End Structure
+
+    ' declare array of structure variables
+    Private priceList(4) As ProductInfo
+
+    Private Sub btnGet_Click(sender As Object, e As EventArgs) Handles btnGet.Click
+        ' display an item's price
+
+        Dim strSearchId As String
+        Dim intSub As Integer
+        Dim dblItemPrice As Double
+
+        strSearchId = txtId.Text
+
+        ' search the pricelist array until the
+        ' end of the array or the ID is found
+        Do Until intSub = priceList.Length OrElse
+            priceList(intSub).strId = strSearchId
+            intSub += 1
+        Loop
+
+        If intSub < priceList.Length Then
+            dblItemPrice = priceList(intSub).dblPrice
+            If chkDisc.Checked Then
+                dblItemPrice *= 0.9
+            End If
+            lblPrice.Text = dblItemPrice.ToString("c2")
+        Else
+            MessageBox.Show("Invalid ID", "Paper Warehouse",
+                            MessageBoxButtons.OK,
+                            MessageBoxIcon.Information)
+        End If
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub txtId_Enter(sender As Object, e As EventArgs) Handles txtId.Enter
+        txtId.SelectAll()
+    End Sub
+
+    Private Sub ClearPrice(sender As Object, e As EventArgs) Handles txtId.TextChanged, chkDisc.CheckedChanged
+        lblPrice.Text = String.Empty
+    End Sub
+End Class
