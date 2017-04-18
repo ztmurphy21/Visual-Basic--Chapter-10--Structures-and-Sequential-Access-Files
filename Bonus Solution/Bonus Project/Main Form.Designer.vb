3d02a33bf53da6ccef331390d7a51754295acb59
Zachary Murphy <ztmurphy21@gmail.com>
2017-04-18 17:57:11 -0400

    assignment 5

---
+++ Bonus Solution/Bonus Project/Main Form.Designer.vb
@@ -0,0 +1,127 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtBonus = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.btnDisplay = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.lstContents = New System.Windows.Forms.ListBox()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(21, 27)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(94, 17)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "&Bonus amount:"
+        '
+        'txtBonus
+        '
+        Me.txtBonus.Location = New System.Drawing.Point(24, 47)
+        Me.txtBonus.Name = "txtBonus"
+        Me.txtBonus.Size = New System.Drawing.Size(74, 25)
+        Me.txtBonus.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(21, 87)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(99, 17)
+        Me.Label2.TabIndex = 5
+        Me.Label2.Text = "Contents of file:"
+        '
+        'btnSave
+        '
+        Me.btnSave.Location = New System.Drawing.Point(189, 27)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(88, 35)
+        Me.btnSave.TabIndex = 2
+        Me.btnSave.Text = "&Save"
+        Me.btnSave.UseVisualStyleBackColor = True
+        '
+        'btnDisplay
+        '
+        Me.btnDisplay.Location = New System.Drawing.Point(189, 69)
+        Me.btnDisplay.Name = "btnDisplay"
+        Me.btnDisplay.Size = New System.Drawing.Size(88, 35)
+        Me.btnDisplay.TabIndex = 3
+        Me.btnDisplay.Text = "&Display"
+        Me.btnDisplay.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(189, 111)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(88, 35)
+        Me.btnExit.TabIndex = 4
+        Me.btnExit.Text = "E&xit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'lstContents
+        '
+        Me.lstContents.Font = New System.Drawing.Font("Courier New", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lstContents.FormattingEnabled = True
+        Me.lstContents.ItemHeight = 16
+        Me.lstContents.Location = New System.Drawing.Point(24, 107)
+        Me.lstContents.Name = "lstContents"
+        Me.lstContents.SelectionMode = System.Windows.Forms.SelectionMode.None
+        Me.lstContents.Size = New System.Drawing.Size(96, 180)
+        Me.lstContents.TabIndex = 6
+        '
+        'frmMain
+        '
+        Me.AcceptButton = Me.btnSave
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 17.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(304, 304)
+        Me.Controls.Add(Me.lstContents)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnDisplay)
+        Me.Controls.Add(Me.btnSave)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txtBonus)
+        Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("Segoe UI", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Margin = New System.Windows.Forms.Padding(3, 5, 3, 5)
+        Me.Name = "frmMain"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Company Bonus"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents txtBonus As System.Windows.Forms.TextBox
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents btnSave As System.Windows.Forms.Button
+    Friend WithEvents btnDisplay As System.Windows.Forms.Button
+    Friend WithEvents btnExit As System.Windows.Forms.Button
+    Friend WithEvents lstContents As System.Windows.Forms.ListBox
+
+End Class
---
+++ Bonus Solution/Bonus Project/Main Form.vb
@@ -0,0 +1,80 @@
+﻿' Name:         Bonus Project
+' Purpose:      Writes bonus amounts to a sequential access file.
+'               Also reads the file and displays its contents.
+' Programmer:   Zach Murphy on 4-18-2017
+
+Option Explicit On
+Option Strict On
+Option Infer Off
+
+Public Class frmMain
+    Private Sub btnDisplay_Click(sender As Object, e As EventArgs) Handles btnDisplay.Click
+        'reads info from bonus.txt
+        'displays info
+
+        'declarations
+        Dim inFile As IO.StreamReader
+        Dim strBonus As String
+
+        'clear any previous bonuses
+        lstContents.Items.Clear()
+
+        'make sure file exists
+        If IO.File.Exists("bonus.txt") Then
+            'open file
+            inFile = IO.File.OpenText("bonus.txt")
+            'loop for reading till end of file
+            Do Until inFile.Peek = -1
+                'read bonus
+                strBonus = inFile.ReadLine
+                'add bonus to box
+                lstContents.Items.Add(strBonus)
+            Loop
+            'closes file
+            inFile.Close()
+        Else
+            MessageBox.Show("Can't find the file", "Company Bonuses", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End If
+    End Sub
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        Me.Close()
+    End Sub
+
+    Private Sub btnSave_Click(sender As Object, e As EventArgs) Handles btnSave.Click
+        'this function saves input data to bonus.txt in bin/debug
+
+
+        'declare file var
+        Dim outFile As IO.StreamWriter
+
+        'open file
+        outFile = IO.File.AppendText("bonus.txt")
+        'write the name on a seperate line
+        outFile.WriteLine(txtBonus.Text)
+        'close the file
+        outFile.Close()
+
+        'clear list box
+        lstContents.Items.Clear()
+
+        'set focus to bonuus input box
+        txtBonus.Focus()
+    End Sub
+
+    Private Sub txtBonus_Enter(sender As Object, e As EventArgs) Handles txtBonus.Enter
+        txtBonus.SelectAll()
+    End Sub
+
+    Private Sub txtBonus_KeyPress(sender As Object, e As KeyPressEventArgs) Handles txtBonus.KeyPress
+        ' allows the text box to accept only numbers, the period, and the Backspace key
+
+        If (e.KeyChar < "0" OrElse e.KeyChar > "9") AndAlso e.KeyChar <> "." AndAlso e.KeyChar <> ControlChars.Back Then
+            e.Handled = True
+        End If
+    End Sub
+
+    Private Sub txtBonus_TextChanged(sender As Object, e As EventArgs) Handles txtBonus.TextChanged
+        lstContents.Items.Clear()
+    End Sub
+End Class
